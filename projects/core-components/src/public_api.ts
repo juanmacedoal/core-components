8de967d4cfb0ee2f07222501ea58332e25f7ee51
MACEDO ALMEIDA Juan Manuel <juan-manuel.macedo@soprasteria.com>
2019-08-01 13:46:33 +0200

    uploaded at npm registry

--- projects/core-components/src/public_api.ts
+++ projects/core-components/src/public_api.ts
@@ -5,5 +5,14 @@
 export * from './lib/core-components.service';
 export * from './lib/core-components.module';
 export * from './lib/lang/lang.module';
+export * from './lib/lang/lang.service';
 export * from './lib/logger/logger.service';
+export * from './lib/api/api.service';
+export * from './lib/api/interceptor/api-interceptor.service';
+export * from './lib/error/error-handler.service';
+export * from './lib/error/interceptor/error-interceptor.service';
+export * from './lib/initializr/config/config.service';
+export * from './lib/initializr/config/config';
+export * from './lib/initializr/config/config.factory';
+export * from './lib/store/store.service';
 
